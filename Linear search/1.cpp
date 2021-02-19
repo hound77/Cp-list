f44d7bc7187fcd3cce586aa7c83e25969c1abbd5
hound77 <yogeshjha151@gmail.com>
2021-02-19 13:17:13 +0530

    Update 1.cpp

--- Linear search/1.cpp
+++ Linear search/1.cpp
@@ -1,1 +1,28 @@
-https://www.hackerearth.com/practice/algorithms/searching/linear-search/practice-problems/algorithm/simple-search-4/
+//https://www.hackerearth.com/practice/algorithms/searching/linear-search/practice-problems/algorithm/simple-search-4/
+    
+    
+    #include<iostream>
+    using namespace std;
+    int main()
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        int k;
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+    }
+         cin>>k;
+         for(int i=0;i<n;i++)
+         {
+             if(a[i]==k)
+             {
+                 cout<<i<<endl;
+             }
+             else
+             {
+                 continue;
+             }
+         }
+    }
